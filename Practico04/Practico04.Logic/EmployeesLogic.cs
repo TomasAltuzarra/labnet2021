3dde8f8081f227a9daf6a6bd2d3c94680512bd90
TomasAltuzarra <e.tomasaltuzarra@gmail.com>
2021-09-20 12:08:56 -0300

    Correccion de formato

---
+++ Practico04/Practico04.Logic/EmployeesLogic.cs
@@ -0,0 +1,25 @@
+﻿using Practico04.Data;
+using Practico04.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practico04.Logic
+{
+    class EmployeesLogic
+    {
+        private readonly NorthwindContext context;
+
+        public EmployeesLogic()
+        {
+            context = new NorthwindContext();
+        }
+
+        public List<Employees> GetAll()
+        {
+            return context.Employees.ToList();
+        }
+    }
+}
---
+++ Practico04/Practico04.UI/Program.cs
@@ -0,0 +1,37 @@
+﻿using Practico04.Entities;
+using Practico04.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practico04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CustomersLogic customersLogic = new CustomersLogic();
+
+            foreach (Customers customer in customersLogic.GetAll())
+            {
+                Console.WriteLine($"{customer.ContactName} - {customer.CompanyName}");
+            }
+
+            Console.ReadLine();
+        }
+
+        public void Menu()
+        {
+            Console.WriteLine("1-Customers\n2-Employees");
+            int opc = int.Parse(Console.ReadLine());
+
+            case(opc){ 
+                case 1:
+                    
+                    } 
+        }
+    }
+}
