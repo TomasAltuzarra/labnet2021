c8d7a531e0dc542f92d81bc98bf9548d9d97e199
TomasAltuzarra <e.tomasaltuzarra@gmail.com>
2021-09-20 11:44:14 -0300

    Primer commit
    Primer commit con conexion a db y armado de un ClassLogic

---
+++ Practico04.Logic/CustomersLogic.cs
@@ -0,0 +1,25 @@
+﻿using Practico04.Data;
+using Practico04.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practico04.Logic
+{
+    public class CustomersLogic
+    {
+        private readonly NorthwindContext context;
+
+        public CustomersLogic()
+        {
+            context = new NorthwindContext(); 
+        }
+
+        public List<Customers> GetAll()
+        {
+            return context.Customers.ToList();
+        }
+    }
+}
