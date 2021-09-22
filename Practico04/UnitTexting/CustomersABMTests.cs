5702b26c078cef756d2545756724403a33e06e31
TomasAltuzarra <e.tomasaltuzarra@gmail.com>
2021-09-22 16:53:02 -0300

    Agregado Plantilla UnitTest
    Agregado UnitTesting

---
+++ Practico04/UnitTexting/CustomersABMTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTexting
+{
+    [TestClass]
+    public class CustomersABMTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
