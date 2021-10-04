11c315818a7aac5e0ab270d9d1bab5e40545d3f2
TomasAltuzarra <e.tomasaltuzarra@gmail.com>
2021-10-04 15:39:04 -0300

    Eliminados usings innecesarios

--- Practico04/Practico07.MVC/Models/CNFactView.cs
+++ Practico04/Practico07.MVC/Models/CNFactView.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace Practico07.MVC.Models
+﻿namespace Practico07.MVC.Models
 {
     public class CNFactView
     {
