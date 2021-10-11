c4fd22856ebf34a5c707593b83ed5a20774d7853
TomasAltuzarra <e.tomasaltuzarra@gmail.com>
2021-10-10 21:20:33 -0300

    API Funcional

--- Practico04/Practico08.WebApi/Controllers/CustomersController.cs
+++ Practico04/Practico08.WebApi/Controllers/CustomersController.cs
@@ -2,24 +2,153 @@
 using Practico04.Entities;
 using Practico04.Entities.DTO;
 using Practico04.Logic;
+using Practico07.MVC.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Mvc;
+using System.Web.Http;
+//using System.Web.Mvc;
 
 namespace Practico08.WebApi.Controllers
 {
-    public class CustomersController : Controller
+    public class CustomersController : ApiController
     {
         readonly CustomersLogic logic = new CustomersLogic();
 
+        //Get All Customers
         [HttpGet]
-        public IEnumerable<Customers> Get()
+        public IEnumerable<CustomerView> GetCustomer()
         {
-            using (NorthwindContext context = new NorthwindContext())
+            List<CustomerDto> customers = logic.GetAll();
+
+            List<CustomerView> customersViews = customers.Select(cus => new CustomerView
             {
-                return context.Customers.ToList();
+                CustomerID = cus.CustomerID,
+                CompanyName = cus.CompanyName,
+                ContactName = cus.ContactName,
+                City = cus.City,
+                Country = cus.Country,
+                Phone = cus.Phone
+            }).ToList();
+
+            return customersViews;
+        }
+
+        //Get One Customer
+        [HttpGet]
+        public CustomerView GetCustomer(string id)
+        {
+            CustomerDto customer = logic.GetOne(id);
+            CustomerView cus = new CustomerView()
+            {
+                CustomerID = (customer.CustomerID).ToUpper(),
+                CompanyName = customer.CompanyName,
+                ContactName = customer.ContactName,
+                City = customer.City,
+                Country = customer.Country,
+                Phone = customer.Phone
+            };
+            return cus;
+        }
+
+        //New Customer - OK
+        /*[HttpPost]
+        public IHttpActionResult AddCustomer([FromBody] CustomerDto customer)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var customerEntity = new CustomerDto()
+                    {
+                        CustomerID = (customer.CustomerID).ToUpper(),
+                        CompanyName = customer.CompanyName,
+                        ContactName = customer.ContactName,
+                        City = customer.City,
+                        Country = customer.Country,
+                        Phone = customer.Phone
+                    };
+                    logic.Add(customerEntity);
+                    return Ok(customer);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(e);
+            }
+        }*/
+        [HttpPost]
+        public string AddCustomer([FromBody] CustomerDto customer)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var customerEntity = new CustomerDto()
+                    {
+                        CustomerID = (customer.CustomerID).ToUpper(),
+                        CompanyName = customer.CompanyName,
+                        ContactName = customer.ContactName,
+                        City = customer.City,
+                        Country = customer.Country,
+                        Phone = customer.Phone
+                    };
+                    logic.Add(customerEntity);
+                    return ":D";
+                }
+                else
+                {
+                    return "D: (ModelNotValid (?))";
+                }
+            }
+            catch (Exception)
+            {
+                return "D: (Exception)";
+            }
+        }
+
+        //Delete Customer - OK
+        [HttpDelete]
+        public IHttpActionResult DeleteCustomer(string id)
+        {
+            try
+            {
+                logic.Delete(id);
+                return Ok(id);
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(e);
+            }
+        }
+
+        //Modify Customer
+        [HttpPut]
+        public IHttpActionResult UpdateCustomer(string id)
+        {
+            try
+            {
+                CustomerDto customer = logic.GetOne(id);
+                var customerEntity = new CustomerDto()
+                {
+                    CustomerID = (customer.CustomerID).ToUpper(),
+                    CompanyName = customer.CompanyName,
+                    ContactName = customer.ContactName,
+                    City = customer.City,
+                    Country = customer.Country,
+                    Phone = customer.Phone
+                };
+                logic.Update(customerEntity);
+                return Ok(customer);
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(e);
             }
         }
     }
---
+++ Practico04/Practico08.WebApi/Models/CustomerView.cs
@@ -0,0 +1,12 @@
+﻿namespace Practico07.MVC.Models
+{
+    public class CustomerView
+    {
+        public string CustomerID { get; set; }
+        public string CompanyName { get; set; }
+        public string ContactName { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+    }
+}
